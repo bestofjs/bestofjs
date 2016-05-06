9f2d07dd0fba176c544ff17deb944cd2402243bf
Michael Rambeau <mikeair@gmail.com>
2016-05-07 06:57:16 +0900

    Fix tests

--- test/components/HomeSpec.js
+++ test/components/HomeSpec.js
@@ -1,93 +1,86 @@
-import test from 'tape';
-import React from 'react';
+import test from 'tape'
+import React from 'react'
 import {
   // mount,
   // render,
   shallow
-} from 'enzyme';
+} from 'enzyme'
 
-import setup from '../setup.js';
+import setup from '../setup.js'
 
 // Data
-import data from '../data/projects';
-import { getInitialState } from '../../src/projectData';
+import data from '../data/projects'
+import { getInitialState } from '../../src/projectData'
 
-setup();
+setup()
 
 // Components to check
-import Home from '../../src/components/home/Home';
-import ProjectList from '../../src/components/projects/ProjectList';
-import ProjectCard from '../../src/components/projects/ProjectCard';
+import Home from '../../src/components/home/Home'
+import ProjectList from '../../src/components/projects/ProjectList'
+import ProjectCard from '../../src/components/projects/ProjectCard'
 
-import populate from '../../src/helpers/populate';
+import populate from '../../src/helpers/populate'
+
+function getHotProjects(state) {
+  return state.githubProjects.hotProjects.daily
+    .map(id => state.entities.projects[id])
+    .slice(0, 20)
+    .map(populate(state.entities.tags))
+}
 
 function mapStateToProps(state) {
   const {
     entities: { projects, tags },
     githubProjects: {
-      hotProjectIds,
       popularProjectIds
     }
-  } = state;
+  } = state
 
-  const hotProjects = hotProjectIds
-    .map(id => projects[id])
-    .slice(0, 20)
-    .map(populate(tags));
   const popularProjects = popularProjectIds
     .map(id => projects[id])
     .slice(0, 20)
-    .map(populate(tags));
+    .map(populate(tags))
 
   return {
-    hotProjects,
+    hotProjects: getHotProjects(state),
     popularProjects
-  };
+  }
 }
 
 test('Check <ProjectList>', assert => {
-  const count = 20;
-  const state = getInitialState(data);
-  const {
-    entities: { projects, tags },
-    githubProjects: {
-      hotProjectIds
-    }
-  } = state;
-  const hotProjects = hotProjectIds
-    .map(id => projects[id])
-    .slice(0, count)
-    .map(populate(tags));
+  const count = 20
+  const state = getInitialState(data)
 
   const component = shallow(
     <ProjectList
-      projects = { hotProjects }
+      projects = { getHotProjects(state) }
     />
-  );
-  assert.ok(component, `The component should exist.`);
-  // console.log(component.debug());
-  const foundProjects = component.find(ProjectCard);
-  assert.equal(foundProjects.length, count, `There should be N projects.`);
-  assert.end();
-});
+  )
+  assert.ok(component, `The component should exist.`)
+  // console.log(component.debug())
+  const foundProjects = component.find(ProjectCard)
+  assert.equal(foundProjects.length, count, `There should be N projects.`)
+  assert.end()
+})
 
 test('Check <Home> component', (assert) => {
-  const state = getInitialState(data);
+  const state = getInitialState(data)
 
-  const props = mapStateToProps(state);
+  const props = mapStateToProps(state)
 
   const component = shallow(
     <Home
-      hotProjects = { props.hotProjects }
-      popularProjects = { props.popularProjects }
+      hotProjects={props.hotProjects }
+      popularProjects={props.popularProjects}
+      uiActions={{}}
     />
-  );
+  )
 
-  assert.ok(component, `The component should exist.`);
+  assert.ok(component, `The component should exist.`)
 
-  const lists = component.find(ProjectList);
+  const lists = component.find(ProjectList)
 
-  assert.equal(lists.length, 2, `There should be 2 lists of projects.`);
+  assert.equal(lists.length, 2, `There should be 2 lists of projects.`)
 
-  assert.end();
-});
+  assert.end()
+})
--- test/containers/ProjectPageSpec.js
+++ test/containers/ProjectPageSpec.js
@@ -1,61 +1,51 @@
-import setup from '../setup.js';
-setup();
+import setup from '../setup.js'
+setup()
 
-import test from 'tape';
-import React from 'react';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
+import test from 'tape'
+import React from 'react'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
-import rootReducer from '../../src/reducers';
+import rootReducer from '../../src/reducers'
 
 // Main components to test
-import ProjectPage from '../../src/containers/ProjectPage';
-
-import { getAllTags, getTag, getAllComponents } from '../utils';
-
-import { fetchReadme } from '../../src/actions';
+import ProjectPage from '../../src/containers/ProjectPage'
 
 import {
   mount
-} from 'enzyme';
-import sinon from 'sinon';
+} from 'enzyme'
+import sinon from 'sinon'
 
 // Data
-import data from '../data/projects';
-import { getInitialState } from '../../src/projectData';
+import data from '../data/projects'
+import { getInitialState } from '../../src/projectData'
 
-const id = '56a95b5843bdc81100111331';
+const id = '56a95b5843bdc81100111331'
 
 test('Check <ProjectPage> container', (assert) => {
-  const state1 = getInitialState(data);
-  const routing = {
-    path: `/projects/${id}`
-  };
-  const state2 = Object.assign({}, state1, { routing });
+  const state = getInitialState(data)
+  const finalCreateStore = compose(applyMiddleware(thunk))(createStore)
+  const store = finalCreateStore(rootReducer, state)
 
-  const finalCreateStore = compose(applyMiddleware(thunk))(createStore);
-  const store = finalCreateStore(rootReducer, state2);
-
-  const unsubscribe = store.subscribe(() => {
-    const { name, readme } = store.getState().entities.projects[id];
+  store.subscribe(() => {
+    const { readme } = store.getState().entities.projects[id]
     if (readme) {
-      assert.ok(readme.length > 1000, `There should be a pretty long readme in the project`);
+      assert.ok(readme.length > 1000, `There should be a pretty long readme in the project`)
     } else {
       // assert.fail('No readme found!')
     }
-  });
+  })
 
-  console.log(ProjectPage.prototype);
-  sinon.spy(ProjectPage.prototype, 'componentWillReceiveProps');
+  sinon.spy(ProjectPage.prototype, 'componentWillReceiveProps')
   const component = mount(
     <Provider store={store}>
       <ProjectPage params={{ id }} />
     </Provider>
-  );
-  assert.ok(ProjectPage.prototype.componentWillReceiveProps, 'componentWillReceiveProps should have been called');
+  )
+  assert.ok(ProjectPage.prototype.componentWillReceiveProps, 'componentWillReceiveProps should have been called')
 
-  assert.ok(component, `The component should exist.`);
+  assert.ok(component, `The component should exist.`)
 
-  assert.end();
-});
+  assert.end()
+})
