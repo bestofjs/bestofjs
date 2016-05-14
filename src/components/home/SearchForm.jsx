8115aa4f61c9b54f8b080bc0afebb88d629c2d8c
Michael Rambeau <mikeair@gmail.com>
2016-05-14 18:54:24 +0900

    Search box: trigger a search when enter key is pressed

--- src/components/home/SearchForm.jsx
+++ src/components/home/SearchForm.jsx
@@ -1,36 +1,40 @@
-import React from 'react';
-import debounce from 'lodash/function/debounce';
+import React from 'react'
+import debounce from 'lodash/function/debounce'
 
 const SearchForm = React.createClass({
   getInitialState() {
     return {
       text: this.props.searchText
-    };
+    }
   },
   componentDidMount() {
-    this.emitChangeDelayed = debounce(this.emitChange, 300);
+    this.emitChangeDelayed = debounce(this.emitChange, 300)
   },
   emitChange(text) {
-    this.props.onChange(text);
+    this.props.onChange(text)
   },
   handleChange(e) {
-    const text = e.target.value;
+    const text = e.target.value
     this.setState({
       text
-    });
-    this.emitChangeDelayed(text);
+    })
+    this.emitChangeDelayed(text)
+  },
+  handleSubmit(e) {
+    e.preventDefault()
+    this.emitChange(this.state.text)
   },
   render() {
     return (
-      <div id="searchbox">
+      <form id="searchbox" onSubmit={this.handleSubmit}>
         <input
           type="text"
           onChange={ this.handleChange }
           autoFocus
         />
         <span className="mega-octicon octicon-search icon"></span>
-      </div>
-    );
+      </form>
+    )
   }
-});
-export default SearchForm;
+})
+export default SearchForm
