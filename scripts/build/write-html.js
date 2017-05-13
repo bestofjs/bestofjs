137fe705f2b013a78ac2dd8ca98eb368aaea9bba
michaelrambeau <mikeair@gmail.com>
2017-05-14 07:13:33 +0900

    Fix SSR

--- scripts/build/write-html.js
+++ scripts/build/write-html.js
@@ -1,9 +1,8 @@
-import pify from 'pify'
 import fs from 'fs-extra'
 import path from 'path'
 
 export default function writeHtmlFile (html, filename) {
   const filepath = path.join('www', filename)
-  return pify(fs.outputFile)(filepath)
+  return fs.outputFile(filepath, html)
     .then(() => `${filename} file created (${(html.length / 1024).toFixed()} KB)`)
 }
