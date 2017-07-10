c28cb5c27df42086d521f48203aba56a32f0ae8f
michaelrambeau <mikeair@gmail.com>
2017-07-10 21:57:27 +0900

    Restore Hall of Fame and user content features

--- src/entry.js
+++ src/entry.js
@@ -8,10 +8,7 @@ import React from 'react'
 import { AppContainer } from 'react-hot-loader'
 import { render } from 'react-dom'
 
-import { fetchJSON } from './helpers/fetch'
-
 import configureStore from './store/configureStore'
-import api from '../config/api'
 import { fetchAllReviews } from './actions/reviewActions'
 import { fetchAllLinks } from './actions/linkActions'
 import { fetchAllHeroes } from './actions/hofActions'
@@ -23,7 +20,6 @@ import { fetchProjectsFromAPI, fetchProjectsSuccess } from './actions/entitiesAc
 
 function start () {
   const store = configureStore({})
-  if (process.env.NODE_ENV === 'development') window.store = store
   fetchProjectsFromAPI()
     .then(data => {
       store.dispatch(fetchProjectsSuccess(data))
@@ -34,11 +30,11 @@ function start () {
 
 start()
 
-function dispatchActions({ store }) {
+function dispatchActions ({ store }) {
   if (window.location.pathname !== '/hof/') {
     // default case: render the app as soon as we get project data
     renderApp(store)
-    // store.dispatch(fetchAllHeroes())
+    store.dispatch(fetchAllHeroes())
   } else {
     // if HoF is currently displayed used server-side rendering,
     // render the app AFTER we get HoF data, to avoid display an empty list
@@ -47,38 +43,6 @@ function dispatchActions({ store }) {
         renderApp(store)
       })
   }
-}
-
-// fetchData()
-//   .then(data => {
-//     const state = getInitialState(data)
-//     startRedux(state)
-//   })
-
-// Stylesheets are included here to avoid server-side rendering errors
-require('./stylesheets/main.styl')
-require('./stylesheets/tooltip/balloon.css')
-require('../node_modules/react-select/dist/react-select.css')
-
-// Launch the Redux application once we get data
-function startRedux (state) {
-  const initialState = state
-  const store = configureStore(initialState)
-  if (process.env.NODE_ENV === 'development') window.store = store
-
-  if (window.location.pathname !== '/hof/') {
-    // default case: render the app as soon as we get project data
-    renderApp(store)
-    store.dispatch(fetchAllHeroes())
-  } else {
-    // if HoF is currently displayed used server-side rendering,
-    // render the app AFTER we get HoF data
-    store.dispatch(fetchAllHeroes())
-    .then(() => {
-      renderApp(store)
-    })
-  }
-
   store.dispatch(fetchAllReviews())
   store.dispatch(fetchAllLinks())
 
@@ -88,24 +52,10 @@ function startRedux (state) {
   menu.start()
 }
 
-function fetchData () {
-  const isLocal = window.bestofjs && window.bestofjs.projects
-  return isLocal ? fetchLocalData() : fetchServerData()
-}
-
-function fetchLocalData () {
-  // read data from global `bestofjs` object
-  return Promise.resolve(window.bestofjs.projects)
-}
-
-function fetchServerData () {
-  const url = `${api('GET_PROJECTS')}projects.json`
-  return fetchJSON(url)
-    .then(json => new Promise(resolve => {
-      window.localStorage.setItem('bestofjs_projects', JSON.stringify(json))
-      resolve(json)
-    }))
-}
+// Stylesheets are included here to avoid server-side rendering errors
+require('./stylesheets/main.styl')
+require('./stylesheets/tooltip/balloon.css')
+require('../node_modules/react-select/dist/react-select.css')
 
 function renderComponent (Component, store) {
   render(
