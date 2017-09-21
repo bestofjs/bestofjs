816bec5c70b91f8036c48f2196147b86783e25e2
michaelrambeau <mikeair@gmail.com>
2017-09-21 21:52:26 +0900

    Customize DAILY TRENDS block when there is one yesterdat data

--- src/components/ProjectView/GithubTab/Trends.js
+++ src/components/ProjectView/GithubTab/Trends.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Heatmap from './Heatmap'
 
 import StarDelta from '../../common/utils/StarDelta'
+import StarIcon from '../../common/utils/StarIcon'
 
 // New "DAILY TRENDS" block (Sep. 2017, v0.9)
 // Show the heat map only if we have at least 2 daily deltas to show
@@ -45,14 +46,32 @@ const MonthlyTrends = ({ project }) => {
     const value = stats[item.category]
     return value !== undefined && value !== null
   })
+  const OnlyYesterday = ({ stats }) => {
+    const value = stats.daily
+    if (value === 0) return <div>No star added on GitHub yesterday</div>
+    return value > 0
+      ? <div>
+          {value}
+          <StarIcon /> added yesterday
+        </div>
+      : <div>
+          {value}
+          <StarIcon /> lost yesterday
+        </div>
+    // return <MonthlyTrendsItem item={items[0]} stats={stats} />
+  }
   return (
     <div className="card-section">
-      <p>Stars added on GitHub, per day, on average:</p>
-      <div className="daily-trends-container">
-        {items.map((item, i) =>
-          <MonthlyTrendsItem item={item} key={i} stats={stats} />
-        )}
-      </div>
+      {stats.weekly
+        ? <div>
+            <p>Stars added on GitHub, per day, on average:</p>
+            <div className="daily-trends-container">
+              {items.map((item, i) =>
+                <MonthlyTrendsItem item={item} key={i} stats={stats} />
+              )}
+            </div>
+          </div>
+        : <OnlyYesterday stats={stats} />}
     </div>
   )
 }
