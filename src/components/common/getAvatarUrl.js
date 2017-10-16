9c12285f705dd3f9d3c5d99b663cafb00df34398
michaelrambeau <mikeair@gmail.com>
2017-10-17 06:09:26 +0900

    Store some SVG icons in the project to improve delivery

--- src/components/common/getAvatarUrl.js
+++ src/components/common/getAvatarUrl.js
@@ -1,5 +1,20 @@
+/*
+Return the image URL to be displayed inside the project card
+Can be either :
+* the GitHub owner avatar (by default if no `icon` property is specified)
+* A custom SVG file if project's `icon`property is specified.
+The SVG can be stored locally (inside `www/logos` folder) or in the cloud.
+*/
+
+const isUrl = input => input.startsWith('http')
+
+const formatIconUrl = input => (isUrl(input) ? input : `/logos/${input}`)
+
+const formatOwnerAvatar = (owner_id, size) =>
+  `https://avatars.githubusercontent.com/u/${owner_id}?v=3&s=${size}`
+
 export default function getProjectAvatarUrl(project, size) {
-  const githubAvatarUrl = `https://avatars.githubusercontent.com/u/${project.owner_id}?v=3&s=${size}`
-  const url = project.icon || githubAvatarUrl
-  return url
+  return project.icon
+    ? formatIconUrl(project.icon)
+    : formatOwnerAvatar(project.owner_id, size)
 }
