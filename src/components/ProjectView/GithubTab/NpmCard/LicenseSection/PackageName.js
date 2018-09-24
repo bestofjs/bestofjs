7b7629f5b24a432181f8a4d0fe023327ef14ea53
michaelrambeau <mikeair@gmail.com>
2018-09-24 17:53:25 +0900

    Add `Unlicense`, fix package name for scoped packages

--- src/components/ProjectView/GithubTab/NpmCard/LicenseSection/PackageName.js
+++ src/components/ProjectView/GithubTab/NpmCard/LicenseSection/PackageName.js
@@ -2,7 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const PackageName = ({ name }) => {
-  const [nameOnly, versionOnly] = name.split('@')
+  // const [nameOnly, versionOnly] = name.split('@')
+  const re = /(.*)@(.*)$/
+  const [_, nameOnly, versionOnly] = re.exec(name) //eslint-disable-line no-unused-vars
   const url = `https://npm.im/${nameOnly}`
   return (
     <span>
