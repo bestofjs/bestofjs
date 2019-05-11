055d3073f321537f28303c6499c115b7efe227d4
michaelrambeau <mikeair@gmail.com>
2019-05-11 17:17:39 +0900

    Upgrade to `react-scripts` 3 and React 16.8

--- src/components/common/form/Button/LoadingButton.js
+++ src/components/common/form/Button/LoadingButton.js
@@ -1,8 +1,8 @@
-// import styled from 'styled-components'
+import styled from 'styled-components'
 
 import NormalButton from './NormalButton'
 
-const LoadingButton = NormalButton.extend`
+const LoadingButton = styled(NormalButton)`
   position: relative;
   cursor: default;
   text-shadow: none !important;
