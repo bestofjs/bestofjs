9f214a541c7bab16d02173800c0a91e8ce3ee4a4
michaelrambeau <mikeair@gmail.com>
2019-07-19 08:45:08 +0900

    Disable links and reviews tabs until  User Content API fix

--- src/components/ProjectView/GithubTab/index.js
+++ src/components/ProjectView/GithubTab/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Tabs from '../Tabs'
+// import Tabs from '../Tabs'
 import Header from './Header'
 import ProjectHeader from '../ProjectHeader'
 import ProjectTabsContent from '../ProjectTabsContent'
@@ -8,12 +8,17 @@ import NpmCard from './NpmCard'
 import TrendsCard from './TrendsCard'
 import ReadmeCard from './ReadmeCard'
 
+/*
+Links and Reviews tabs are disabled until the "User Content API" is fixed
+TODO: FIX THE API ASAP!
+*/
+
 const GitHub = props => {
   const { project } = props
   return (
     <div>
       <ProjectHeader project={project} />
-      <Tabs project={project} activePath="" />
+      {/* <Tabs project={project} activePath="" /> */}
       <ProjectTabsContent>
         <Header {...props} />
       </ProjectTabsContent>
