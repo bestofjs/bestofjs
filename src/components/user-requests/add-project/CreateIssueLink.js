59a0e4df15d5231ac51138743390badb5c1dd421
michaelrambeau <mikeair@gmail.com>
2019-12-26 08:22:55 +0900

    Delete unused "User request" component

--- src/components/user-requests/add-project/CreateIssueLink.js
+++ src/components/user-requests/add-project/CreateIssueLink.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import styled from 'styled-components'
 
-import { Button, ExternalLink } from '../../core'
+import { ExternalLink } from '../../core'
 import getApi from '../../../api/config'
 
 const templates = {
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -5,7 +5,7 @@ import asyncComponent from './asyncComponent'
 
 import HomePage from '../pages/home-page'
 import BookmarksPage from '../pages/bookmarks-page'
-import HoFPage from '../pages/HallOfFamePage'
+import HallOfFamePage from '../pages/HallOfFamePage'
 import TagsPage from '../pages/tags-page'
 import NoMatch from './NoMatch'
 import { SearchResultsContainer } from '../components/search/SearchResults'
@@ -16,35 +16,23 @@ const AsyncAboutPage = asyncComponent(() =>
 const AsyncViewProject = asyncComponent(() =>
   import(/* webpackChunkName: "single-project" */ '../pages/ProjectDetails/ProjectDetails')
 )
-const AsyncRequests = asyncComponent(() =>
-  import(/* webpackChunkName: "my-requests" */ './Requests')
-)
 
 const Routes = props => {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
-      {/* <Route
-        path="/projects/trending/:period"
-        component={SearchResultsContainer}
-      /> */}
       <Route exact path="/projects/:id">
         <AsyncViewProject {...props} />
       </Route>
       <Route exact path="/projects" component={SearchResultsContainer} />
       <Redirect from={`/tags/:id`} to={`/projects?tags=:id`} />
       <Route from={`/tags`} component={TagsPage} />
-      <Route
-        exact
-        path="/hall-of-fame"
-        render={ownProps => <HoFPage {...props} {...ownProps} />}
-      />
+      <Route exact path="/hall-of-fame">
+        <HallOfFamePage {...props} />
+      </Route>
       <Redirect from="/hof" to="/hall-of-fame" />
       <Redirect from="/myprojects" to="/bookmarks" />
       <Route path="/bookmarks" component={BookmarksPage} />
-      <Route path="/requests">
-        <AsyncRequests />
-      </Route>
       <Route exact path="/about" component={AsyncAboutPage} />
       <Route component={NoMatch} />
     </Switch>
