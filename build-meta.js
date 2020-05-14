9a26b838798ff5700e1b6e24d0081b5b3bff4c3f
michaelrambeau <michael@medmain.com>
2020-05-14 09:48:04 +0900

    Add missing `await`

--- build-meta.js
+++ build-meta.js
@@ -13,9 +13,9 @@ function main() {
   saveJSON(data, 'meta.json')
 }
 
-function saveJSON(json, fileName) {
+async function saveJSON(json, fileName) {
   const filePath = path.join(process.cwd(), 'build', fileName)
   console.log('Saving', json, filePath)
-  fs.outputJson(filePath, json)
+  await fs.outputJson(filePath, json)
   console.info('JSON file saved')
 }
