af2e7dfbfd10dcf655156d6c95b40446be370074
michaelrambeau <michael@medmain.com>
2020-08-02 14:54:02 +0900

    First E2E test with QAWolf

---
+++ .qawolf/tagSearch.test.ts
@@ -0,0 +1,31 @@
+import { Browser, Page } from 'playwright'
+import qawolf from 'qawolf'
+
+let browser: Browser
+let page: Page
+
+beforeAll(async () => {
+  browser = await qawolf.launch()
+  const context = await browser.newContext()
+  await qawolf.register(context)
+  page = await context.newPage()
+})
+
+afterAll(async () => {
+  await qawolf.stopVideos()
+  await browser.close()
+})
+
+test('tagSearch', async () => {
+  await page.goto('https://bestofjs.org/')
+  await page.click('#main')
+  await page.click('text=Pick tags or enter keywords...')
+  await page.fill('#react-select-2-input', 'frame')
+  await page.click('div:nth-of-type(2) div:nth-of-type(4)')
+  await qawolf.scroll(page, 'html', { x: 0, y: 78 })
+  await page.click('[href="/projects/react"]')
+  await qawolf.scroll(page, 'html', { x: 0, y: 536 })
+  await page.click('text=3 dependencies')
+  await qawolf.scroll(page, 'html', { x: 0, y: 0 })
+  await page.press('body', 'ArrowUp')
+})
---
+++ qawolf.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  config: 'node_modules/qawolf/ts-jest.config.json',
+  rootDir: '.qawolf',
+  testTimeout: 60000,
+  useTypeScript: true
+}
