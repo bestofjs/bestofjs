57155afee1a6a5363fccb69225fcf6a5bd093365
michaelrambeau <michael@medmain.com>
2020-08-14 22:17:21 +0900

    Add more content to the timeline

--- src/pages/timeline-page.tsx
+++ src/pages/timeline-page.tsx
@@ -1,49 +1,41 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import groupBy from 'lodash/groupBy'
-import orderBy from 'lodash/orderBy'
 
 import { MainContent, PageTitle, Spinner } from 'components/core'
 import { allProjects } from 'selectors'
 import { useSelector } from 'containers/project-data-container'
-import { Avatar } from 'components/core/project'
 import { Timeline } from 'components/timeline/timeline'
 
 export const TimelinePage = () => {
   const projects = useSelector(allProjects)
   if (!projects.length) return <Spinner />
-  const byYear: Record<string, BestOfJS.Project[]> = groupBy(
-    projects,
-    getProjectYear
-  )
+
   return (
     <MainContent>
       <PageTitle>Timeline: 2006-2020 in 20 projects</PageTitle>
       <PageDescription>
         Our favorite newsletter{' '}
-        <a href="https://javascriptweekly.com/issues/500">JavaScript Weekly</a>,
+        <a href="https://javascriptweekly.com/issues/500">JavaScript Weekly</a>{' '}
         has just released its 500th issue.
         <br />
         Let's celebrate this milestone by picking 20 significant projects, from
         2006 to 2020.
         <br />A short story of the web platform from <i>jQuery</i> to{' '}
-        <i>Rome</i>.
+        <i>Rome</i>.<br />
+        Click on the links to see the project details and the trends on{' '}
+        <i>Best of JS</i>.
       </PageDescription>
       <Timeline />
-      {false && (
-        <Wrapper>
-          {Object.keys(byYear).map((year: string) => (
-            <YearRow key={year} year={year} projects={byYear[year]} />
-          ))}
-        </Wrapper>
-      )}
       <Disclaimer>
-        <h3>Disclaimer</h3>
+        <h3>About this timeline / Disclaimer</h3>
         <p>We could have mentioned a lot of other projects:</p>
         <ul>
           <li>
             <Link to="/projects/threejs">Three.js</Link> (2010)
+          </li>
+          <li>
+            <Link to="/projects/backbone">Backbone</Link> (2010)
           </li>
           <li>
             <Link to="/projects/meteor">Meteor</Link> (2012)
@@ -55,10 +47,16 @@ export const TimelinePage = () => {
             <Link to="/projects/redux">Redux</Link> (2015)
           </li>
           <li>
+            <Link to="/projects/rollup">Rollup</Link> (2015)
+          </li>
+          <li>
             <Link to="/projects/gatsby">Gatsby</Link> (2015)
           </li>
           <li>
             <Link to="/projects/storybook">Storybook</Link> (2016)
+          </li>
+          <li>
+            <Link to="/projects/parcel">Parcel</Link> (2017)
           </li>
         </ul>
         <p>...but we had to make choices to keep this timeline compact.</p>
@@ -71,6 +69,11 @@ export const TimelinePage = () => {
             We wanted at least one project for every year between 2010 and 2020.
           </li>
         </ul>
+        <p>
+          The date displayed for each project is the date of the creation of the
+          repository on GitHub, except for the following projects: jQuery,
+          Node.js, and TypeScript.
+        </p>
         <p>Thank you for your understanding!</p>
       </Disclaimer>
     </MainContent>
@@ -86,57 +89,6 @@ export const isIncludedInHotProjects = project => {
   )
   return !hasExcludedTag
 }
-
-const YearRow = ({ year, projects }) => {
-  return (
-    <>
-      <YearCell>{year}</YearCell>
-      <YearProjects>
-        {orderBy(projects, ['stars'], ['desc'])
-          .filter(project => !!project.icon)
-          .filter(isIncludedInHotProjects)
-          .slice(0, 10)
-          .map(project => (
-            <div key={project.slug}>
-              <Link
-                to={`/projects/${project.slug}`}
-                className="hint--top"
-                aria-label={`${project.name} ${project.stars} stars`}
-              >
-                <Avatar project={project} size={50} />
-              </Link>
-            </div>
-          ))}
-      </YearProjects>
-    </>
-  )
-}
-
-const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: 100px 1fr;
-  > div {
-    background-color: white;
-    padding: 1rem;
-  }
-`
-const YearCell = styled.div`
-  font-size: 1.5rem;
-  margin-bottom: 1rem;
-  display: flex;
-  align-items: center;
-`
-const YearProjects = styled.div`
-  margin-bottom: 1rem;
-  background-color: white;
-  display: flex;
-  > div {
-    margin-right: 1rem;
-  }
-`
-
-const getProjectYear = (project: BestOfJS.ProjectDetails) =>
-  new Date(project.created_at).getFullYear()
 
 const PageDescription = styled.div`
   padding-left: 1rem;
