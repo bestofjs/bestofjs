332db38f709ef68292d9ef2571b2c77d64b0bb2a
michaelrambeau <michael@medmain.com>
2020-11-17 09:33:45 +0900

    Add quick links section to Project details page

--- src/components/project-details/project-header.tsx
+++ src/components/project-details/project-header.tsx
@@ -1,37 +1,106 @@
 import React from 'react'
+import styled from 'styled-components'
+import { GoHome, GoMarkGithub } from 'react-icons/go'
+import { DiNpm } from 'react-icons/di'
 
+import { Button } from 'components/core'
 import { Avatar } from 'components/core/project'
-import formatUrl from 'helpers/url'
-import { ExternalLink } from 'components/core/typography'
 import { TagLabelGroup } from 'components/tags/tag-label'
+import formatUrl from 'helpers/url'
 
 type Props = { project: BestOfJS.Project }
-export const ProjectHeader = ({ project }: Props) => (
-  <div style={{ marginBottom: '2rem' }}>
-    <div style={{ display: 'flex', alignItems: 'center', margin: '0 0 1rem' }}>
-      <div style={{ flex: 1 }}>
-        <h1 style={{ paddingLeft: '0rem' }}>{project.name}</h1>
-        <div
-          style={{
-            marginTop: '.5rem',
-            borderLeft: '2px solid var(--iconColor)',
-            paddingLeft: '1rem'
-          }}
-        >
-          {project.description}{' '}
-          {project.url && (
-            <ExternalLink url={project.url}>
-              {formatUrl(project.url)}
-            </ExternalLink>
-          )}
+export const ProjectHeader = ({ project }: Props) => {
+  const { full_name, packageName, repository, url } = project
+
+  return (
+    <HeaderLayout>
+      <Main>
+        <div style={{ paddingRight: '1rem' }}>
+          <Avatar project={project} size={75} />
         </div>
-        <div style={{ marginTop: '1rem' }}>
-          <TagLabelGroup tags={project.tags} />
+        <div style={{ flex: 1 }}>
+          <h1>{project.name}</h1>
+          <div style={{ margin: '0.5rem 0' }}>{project.description}</div>
+          <div>
+            <TagLabelGroup tags={project.tags} />
+          </div>
         </div>
-      </div>
-      <div style={{ fontSize: '1.25rem' }}>
-        <Avatar project={project} size={75} />
-      </div>
-    </div>
-  </div>
-)
+      </Main>
+      <QuickLinks>
+        <Button as="a" href={repository}>
+          <GoMarkGithub size={20} className="icon" />
+          <span>{full_name}</span>
+        </Button>
+        {url && (
+          <Button as="a" href={url}>
+            <GoHome size={20} className="icon" />
+            <span>{formatUrl(url)}</span>
+          </Button>
+        )}
+        {packageName && (
+          <Button as="a" href={`https://www.npmjs.com/package/${packageName}`}>
+            <DiNpm
+              size={28}
+              className="icon"
+              style={{ transform: 'translateY(2px)' }}
+            />
+            <span>{packageName}</span>
+          </Button>
+        )}
+      </QuickLinks>
+    </HeaderLayout>
+  )
+}
+
+const breakpoint = 800
+
+const HeaderLayout = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  flex-direction: column;
+  margin-bottom: 2rem;
+  @media (min-width: ${breakpoint}px) {
+    flex-direction: row;
+    align-items: center;
+  }
+`
+
+const Main = styled.div`
+  display: flex;
+  align-items: center;
+  flex-grow: 1;
+  min-height: 120px;
+  @media (min-width: ${breakpoint}px) {
+    padding-right: 1rem;
+    border-right: 1px dashed var(--iconColor);
+  }
+`
+
+const QuickLinks = styled.aside`
+  position: relative;
+  margin-top: 1rem;
+  @media (min-width: ${breakpoint}px) {
+    width: 280px;
+    margin-top: 0;
+    padding-left: 1rem;
+  }
+  > * {
+    width: 100%;
+    margin-bottom: 0.5rem;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  span {
+    padding-left: 36px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+  a {
+    justify-content: flex-start;
+  }
+  .icon {
+    position: absolute;
+  }
+`
