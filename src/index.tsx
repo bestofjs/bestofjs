50eb294e27f217d322d9bd6aaf47795524a9289a
michaelrambeau <michael@medmain.com>
2021-06-20 10:23:32 +0900

    Add ChakraUI provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,40 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
 
 import { unregister } from './registerServiceWorker'
 import { Root } from './root'
 
 // Old-fashioned stylesheets
-import './stylesheets/base.css'
+// import './stylesheets/base.css'
+
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: 'light'
+}
+
+// 3. extend the theme
+const customTheme = extendTheme({
+  config,
+  styles: {
+    global: props => ({
+      body: {
+        bg: mode('gray.100', 'gray.700')(props)
+      }
+    })
+  }
+})
+
+console.log(customTheme)
 
 function start() {
-  render(<Root />, document.getElementById('root'))
+  render(
+    <ChakraProvider theme={customTheme}>
+      <Root />
+    </ChakraProvider>,
+    document.getElementById('root')
+  )
 }
 
 start()
