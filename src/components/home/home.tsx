b00b8f2b181c5d53fa50ae9fb9f50d19b34b106a
michaelrambeau <michael@medmain.com>
2021-11-01 08:54:47 +0900

    Remove "Hello dark mode fans!" message from top page

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -26,7 +26,6 @@ import { HotProjects, NewestProjects } from "./home-projects";
 import { RandomFeaturedProject } from "./featured-projects";
 import { Row, MainColumn, RightSideBar } from "./layout";
 import { HomeMonthlyRankings } from "./home-monthly-rankings";
-import { DarkModeNews } from "./dark-mode-news";
 
 export const Home = (props) => {
   log("Render the <Home> component");
@@ -34,16 +33,15 @@ export const Home = (props) => {
 
   return (
     <MainContent>
+      <PageHeader title="The best of JavaScript, HTML and CSS" />
       <Section>
         <Row>
           <MainColumn>
-            <PageHeader title="The best of JavaScript, HTML and CSS" />
             <HotProjects {...props} />
             {!pending && <NewestProjects {...props} />}
           </MainColumn>
           {!pending && (
             <RightSideBar>
-              <DarkModeNews />
               <RandomFeaturedProject />
               <Section.Header icon={<GoTag fontSize={32} />}>
                 <Section.Title>Popular Tags</Section.Title>
