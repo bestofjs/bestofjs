bestofjs/bestofjs

commit 9dae96e19614e1d6a1d115c08d4e43f277434c6c

Clean and simplify the code (#119)

src/components/tags/project-tag.tsx
@@ -3,13 +3,17 @@ import { Link } from "react-router-dom";
 import { MdAdd } from "react-icons/md";
 
 import { Button, Wrap, WrapItem, useColorMode } from "components/core";
-import { useSearch, updateLocation } from "../search";
+import { useNextLocation } from "../search";
 
-export const ProjectTagGroup = ({ tags, ...otherProps }) => {
+type Props = {
+  tags: BestOfJS.Tag[];
+  appendTag?: boolean;
+};
+export const ProjectTagGroup = ({ tags, ...otherProps }: Props) => {
   return (
     <Wrap>
       {tags.map((tag) => (
-        <WrapItem key={tag.id}>
+        <WrapItem key={tag.code}>
           <ProjectTag tag={tag} {...otherProps} />
         </WrapItem>
       ))}
@@ -17,28 +21,28 @@ export const ProjectTagGroup = ({ tags, ...otherProps }) => {
   );
 };
 
-export const ProjectTag = ({ tag, baseTagIds = [] }) => {
-  const { location } = useSearch();
+export const ProjectTag = ({
+  tag,
+  appendTag,
+}: {
+  tag: BestOfJS.Tag;
+  appendTag?: boolean;
+}) => {
   const { colorMode } = useColorMode();
+  const { updateLocation } = useNextLocation();
+  const nextLocation = updateLocation((state) => ({
+    ...state,
+    selectedTags: appendTag ? [...state.selectedTags, tag.code] : [tag.code],
+  }));
 
-  const isMultiTagLink = baseTagIds.length > 0;
-
-  const nextLocation = updateLocation(
-    { ...location, pathname: "/projects" },
-    {
-      query: "",
-      selectedTags: [...baseTagIds, tag.id],
-      page: 1,
-    }
-  );
   return (
     <Button
       as={Link}
-      to={nextLocation}
+      to={{ ...nextLocation, pathname: "/projects" }}
       variant={colorMode === "dark" ? "solid" : "outline"}
       size="sm"
       fontSize="0.875rem"
-      rightIcon={isMultiTagLink ? <MdAdd /> : undefined}
+      rightIcon={appendTag ? <MdAdd /> : undefined}
     >
       {tag.name}
     </Button>

.env
@@ -1,0 +1,7 @@
+# This file is tracked in Git to make the initial setup simpler
+# To override values when running the API in local, create a `.env.local` file (not tracked by Git)
+# About priorities, see https://create-react-app.dev/docs/adding-custom-environment-variables/#what-other-env-files-can-be-used
+REACT_APP_JSON_API=https://bestofjs-static-api.vercel.app
+REACT_APP_DYNAMIC_API=https://bestofjs-serverless.now.sh
+REACT_APP_FETCH_RANKINGS=https://bestofjs-rankings.vercel.app
+REACT_APP_VERSION=$npm_package_version

.gitignore
@@ -1,5 +1,5 @@
-# Secret
-.env
+# Override values from .env files (used to run the API in local)
+.env.local
 
 # Build
 build

src/App.test.js
@@ -1,25 +1,0 @@
-import React from "react";
-import { Router } from "react-router-dom";
-import ReactDOM from "react-dom";
-import { createMemoryHistory } from "history";
-
-import { AuthProvider } from "containers/auth-container";
-import { ProjectDataProvider } from "containers/project-data-container";
-import { App } from "./app";
-
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  const history = createMemoryHistory();
-
-  ReactDOM.render(
-    <Router history={history}>
-      <AuthProvider>
-        <ProjectDataProvider>
-          <App />
-        </ProjectDataProvider>
-      </AuthProvider>
-    </Router>,
-    div
-  );
-  ReactDOM.unmountComponentAtNode(div);
-});

src/api/config.ts
@@ -1,33 +1,0 @@
-const baseSetup = {
-  GET_PROJECTS:
-    readEnvironmentVariable("STATIC_API") ||
-    "https://bestofjs-static-api.vercel.app",
-  GET_README: "https://bestofjs-serverless.now.sh",
-  GET_PROJECT_DETAILS: "https://bestofjs-serverless.now.sh",
-  GET_PACKAGE_DATA: "https://bestofjs-serverless.now.sh",
-  GET_RANKINGS: "https://bestofjs-rankings.vercel.app",
-  ISSUES_REPO: "michaelrambeau/bestofjs",
-};
-
-const setup = {
-  development: {
-    ...baseSetup,
-    // GET_RANKINGS: 'http://localhost:3000'
-  },
-  production: {
-    ...baseSetup,
-  },
-};
-
-export default function getApiRootURL(key) {
-  const { NODE_ENV } = process.env;
-  const isLocalEnv = NODE_ENV === "development";
-  const api = isLocalEnv ? setup.development : setup.production;
-  if (!api[key]) throw new Error(`No API end point defined for ${key}`);
-  return api[key];
-}
-
-function readEnvironmentVariable(key) {
-  // See https://create-react-app.dev/docs/adding-custom-environment-variables/#docsNav
-  return process.env[`REACT_APP_${key}`];
-}

src/api/hooks.ts
@@ -1,7 +1,12 @@
 import useSWR from "swr";
 
-import getApiRootURL from "./config";
-import { fetchHTML, fetchJSON } from "../helpers/fetch";
+import {
+  FETCH_DETAILS_URL,
+  FETCH_PACKAGE_DATA_URL,
+  FETCH_RANKINGS_URL,
+  FETCH_README_URL,
+} from "config";
+import { fetchHTML, fetchJSON } from "helpers/fetch";
 
 export function useFetchProjectReadMe({ full_name, branch }) {
   const fetcher = () => fetchProjectReadMe({ full_name, branch });
@@ -9,9 +14,7 @@ export function useFetchProjectReadMe({ full_name, branch }) {
 }
 
 function fetchProjectReadMe({ full_name, branch = "master" }) {
-  const url = `${getApiRootURL(
-    "GET_README"
-  )}/api/project-readme?fullName=${full_name}&branch=${branch}`;
+  const url = `${FETCH_README_URL}/api/project-readme?fullName=${full_name}&branch=${branch}`;
   return fetchHTML(url);
 }
 
@@ -21,26 +24,8 @@ export function useFetchProjectDetails({ full_name }) {
 }
 
 function fetchProjectDetails({ full_name }) {
-  const url = `${getApiRootURL(
-    "GET_PROJECT_DETAILS"
-  )}/api/project-details?fullName=${full_name}`;
+  const url = `${FETCH_DETAILS_URL}/api/project-details?fullName=${full_name}`;
   return fetchJSON(url);
-}
-
-const loadNewsletterIssue = ({ issueNumber: number }) => {
-  const root = "https://weekly.bestofjs.org";
-  const url = number
-    ? `${root}/issues/${number}/routeInfo.json`
-    : `${root}/latest/routeInfo.json`;
-  return fetchJSON(url).then(({ data }) => ({
-    isLatest: data.isLatest,
-    ...data.issue,
-  }));
-};
-
-export function useWeeklyNewsletter(issueNumber) {
-  const fetcher = () => loadNewsletterIssue({ issueNumber });
-  return useSWR(["/api/newsletters/issue", issueNumber], fetcher);
 }
 
 export function useFetchMonthlyDownloads(packageName) {
@@ -49,9 +34,7 @@ export function useFetchMonthlyDownloads(packageName) {
 }
 
 const fetchMonthlyDownloads = ({ packageName }) => {
-  const url = `${getApiRootURL(
-    "GET_PACKAGE_DATA"
-  )}/api/package-monthly-downloads?packageName=${packageName}`;
+  const url = `${FETCH_PACKAGE_DATA_URL}/api/package-monthly-downloads?packageName=${packageName}`;
   return fetchJSON(url);
 };
 
@@ -67,8 +50,9 @@ type MonthlyDate = {
 };
 
 function loadMonthlyRankings(date?: MonthlyDate) {
-  const rootURL = getApiRootURL("GET_RANKINGS");
-  const url = `${rootURL}/monthly/${date ? formatDate(date) : "latest"}`;
+  const url = `${FETCH_RANKINGS_URL}/monthly/${
+    date ? formatDate(date) : "latest"
+  }`;
   return fetchJSON(url);
 }
 

src/components/core/index.js
@@ -4,6 +4,8 @@ export * from "./empty-content";
 export * from "./html-head";
 export * from "./layout";
 export * from "./main-content";
+export * from "./project";
+export * from "./project-avatar";
 export * from "./section";
 export * from "./spinner";
 export * from "./typography";

src/components/core/pagination/pagination-controls.js
@@ -8,14 +8,15 @@ import {
   ChevronRightIcon,
   ChevronLeftIcon,
 } from "../icons";
-import { updateLocation } from "../../search/search-utils";
+import { useNextLocation } from "components/search/search-utils";
 import { PaginationContainer } from "./provider";
 
 const iconSize = 28;
 
-export const TopPaginationControls = ({ history, location }) => {
+export const TopPaginationControls = () => {
   const { from, to, currentPageNumber, total, hasPreviousPage, hasNextPage } =
     PaginationContainer.useContainer();
+  const { navigate } = useNextLocation();
 
   return (
     <Flex alignItems="center">
@@ -32,21 +33,13 @@ export const TopPaginationControls = ({ history, location }) => {
       <HStack>
         <PaginationButton
           isDisabled={!hasPreviousPage}
-          onClick={() =>
-            history.push(
-              updateLocation(location, { page: currentPageNumber - 1 })
-            )
-          }
+          onClick={() => navigate({ page: currentPageNumber - 1 })}
         >
           <ChevronLeftIcon size={iconSize} />
         </PaginationButton>
         <PaginationButton
           isDisabled={!hasNextPage}
-          onClick={() =>
-            history.push(
-              updateLocation(location, { page: currentPageNumber + 1 })
-            )
-          }
+          onClick={() => navigate({ page: currentPageNumber + 1 })}
         >
           <ChevronRightIcon size={iconSize} />
         </PaginationButton>
@@ -55,7 +48,7 @@ export const TopPaginationControls = ({ history, location }) => {
   );
 };
 
-export const BottomPaginationControls = ({ history, location }) => {
+export const BottomPaginationControls = () => {
   const {
     currentPageNumber,
     hasPreviousPage,
@@ -63,13 +56,14 @@ export const BottomPaginationControls = ({ history, location }) => {
     lastPageNumber,
     pageNumbers,
   } = PaginationContainer.useContainer();
+  const { navigate } = useNextLocation();
 
   return (
     <HStack mt={8} w="100%" justifyContent="flex-end">
       {pageNumbers.length > 2 && (
         <PaginationButton
           disabled={currentPageNumber === 1}
-          onClick={() => history.push(updateLocation(location, { page: 1 }))}
+          onClick={() => navigate({ page: 1 })}
         >
           <DoubleChevronLeftIcon size={iconSize} />
         </PaginationButton>
@@ -77,22 +71,14 @@ export const BottomPaginationControls = ({ history, location }) => {
 
       <PaginationButton
         disabled={!hasPreviousPage}
-        onClick={() =>
-          history.push(
-            updateLocation(location, { page: currentPageNumber - 1 })
-          )
-        }
+        onClick={() => navigate({ page: currentPageNumber - 1 })}
       >
         <ChevronLeftIcon size={iconSize} />
       </PaginationButton>
 
       <PaginationButton
         disabled={!hasNextPage}
-        onClick={() =>
-          history.push(
-            updateLocation(location, { page: currentPageNumber + 1 })
-          )
-        }
+        onClick={() => navigate({ page: currentPageNumber + 1 })}
       >
         <ChevronRightIcon size={iconSize} />
       </PaginationButton>
@@ -100,9 +86,7 @@ export const BottomPaginationControls = ({ history, location }) => {
       {pageNumbers.length > 2 && (
         <PaginationButton
           disabled={currentPageNumber === lastPageNumber}
-          onClick={() =>
-            history.push(updateLocation(location, { page: lastPageNumber }))
-          }
+          onClick={() => navigate({ page: lastPageNumber })}
         >
           <DoubleChevronRightIcon size={iconSize} />
         </PaginationButton>

commit 5312834b9c213dbd7c4d8ddb2cf3612fe5e416fb

Reset search `query` when clicking on tag links (problem introduced by #119)

src/components/tags/project-tag.tsx
@@ -33,6 +33,7 @@ export const ProjectTag = ({
   const nextLocation = updateLocation((state) => ({
     ...state,
     selectedTags: appendTag ? [...state.selectedTags, tag.code] : [tag.code],
+    query: "",
   }));
 
   return (