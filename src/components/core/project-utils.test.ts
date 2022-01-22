92a8ab64b3503d4d297534903e7ad1587088d59e
Michael Rambeau <mikeair@gmail.com>
2022-01-22 20:15:36 +0900

    Generate custom project slug, without relying on `slugify` package (#122)

---
+++ src/components/core/project-utils.test.ts
@@ -0,0 +1,13 @@
+import { slugify } from "./project";
+
+it("Should generate the right slug", () => {
+  expect(slugify("Vue.js")).toEqual("vuejs");
+  expect(slugify("React Native")).toEqual("react-native");
+  expect(slugify("date-fns")).toEqual("date-fns");
+  expect(slugify("Angular 1")).toEqual("angular-1");
+  expect(slugify("$mol")).toEqual("$mol");
+  expect(slugify("You don't know JS")).toEqual("you-dont-know-js");
+  expect(slugify("JS Algorithms & Data Structures")).toEqual(
+    "js-algorithms-and-data-structures"
+  );
+});
