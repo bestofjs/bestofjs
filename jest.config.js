9efe12a41681644cfb91e89fd3a72fdda78bd403
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-05-21 16:11:38 +0700

    feat: Integrate Jest with SWC compiler

---
+++ jest.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/mocks/**",
+    "!src/test-utils/**",
+  ],
+  coveragePathIgnorePatterns: [],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  testEnvironment: "jsdom",
+  modulePaths: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.(ts|js|tsx|jsx)$": "@swc/jest",
+    "^.+\\.css$": "jest-preview/transforms/css",
+    "^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)":
+      "jest-preview/transforms/file",
+  },
+  transformIgnorePatterns: [
+    "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$",
+  ],
+  modulePaths: ["<rootDir>/src"],
+  moduleNameMapper: {
+    "^react-native$": "react-native-web",
+  },
+  moduleFileExtensions: [
+    // Place tsx and ts to beginning as suggestion from Jest team
+    // https://jestjs.io/docs/configuration#modulefileextensions-arraystring
+    "tsx",
+    "ts",
+    "web.js",
+    "js",
+    "web.ts",
+    "web.tsx",
+    "json",
+    "web.jsx",
+    "jsx",
+    "node",
+  ],
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname",
+  ],
+  resetMocks: true,
+};
