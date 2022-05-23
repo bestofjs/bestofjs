a2db19232fba050ffe78a3c2724086dac052c1c1
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-05-24 00:55:56 +0700

    Add outline for Tags test suite

--- src/__tests__/Tags.test.tsx
+++ src/__tests__/Tags.test.tsx
@@ -1,5 +1,32 @@
 export {};
 
 describe("Tags", () => {
-  it.todo("should render correctly");
+  it("should render correctly", () => {
+    // Navigate to Tags page
+
+    // Sort by number of project should be the default option
+
+    // Should have 20 tags
+
+    // "React" should be at the top of the list
+
+    // Sort by alphabetical order
+
+    // "3D" should be at the top of the list
+
+    // Previous page should be disabled
+
+    // Next page should be enabled
+    // Click Next page
+
+    // "Building tool" should be at the top of the list
+
+    // Click "Last Page"
+
+    // "XML" should be at the bottom of the list
+
+    // Click "First Page"
+
+    // "Browser storage" should be at the bottom of the list
+  });
 });
