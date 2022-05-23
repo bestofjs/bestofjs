6f79f796730f107d8d2140aa83feacc737ab14b2
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-05-24 01:01:29 +0700

    Add outline for Projects test suite

--- src/__tests__/Projects.test.tsx
+++ src/__tests__/Projects.test.tsx
@@ -1,5 +1,49 @@
 export {};
 
 describe("Projects", () => {
-  it.todo("should render correctly");
+  it("should render correctly", () =>{
+    // Navigate to Projects page
+
+    // Sort by number of stars should be the default option
+
+    // Should have 30 projects
+
+    // "freeCodeCamp" should be at the top of the list
+
+    // Click Next Page
+    // "json-server" should be at the bottom of the list
+
+    // Click Last Page
+    // "Your First RPG" should be at the bottom of the list
+
+    // Click First Page
+    // "Clean Code" should be at the bottom of the list
+
+    // Sort by stars added yesterday
+    // "ABC" should be at the top of the list
+
+    // Sort by stars the last 7 days
+    // "ABC" should be at the top of the list
+
+    // Sort by stars the last 30 days
+    // "ABC" should be at the top of the list
+
+    // Sort by stars the last 12 months
+    // "ABC" should be at the top of the list
+
+    // Sort by downloads the last 30 days
+    // "ABC" should be at the top of the list
+
+    // Sort by date of the latest commit
+    // "ABC" should be at the top of the list
+
+    // Sort by number of contributors
+    // "ABC" should be at the top of the list
+
+    // Sort by date of creation (Oldest first)
+    // "ABC" should be at the top of the list
+
+    // Sort by date of addition on Best of JS
+    // "ABC" should be at the top of the list
+  });
 });
