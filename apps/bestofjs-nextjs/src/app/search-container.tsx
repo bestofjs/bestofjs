40bd99e0cd340e30f693d2e5db3ee75f5da2c71e
michaelrambeau <mikeair@gmail.com>
2023-08-27 17:58:59 +0900

    Don't refetch search data when the browser tag get the focus

--- apps/bestofjs-nextjs/src/app/search-container.tsx
+++ apps/bestofjs-nextjs/src/app/search-container.tsx
@@ -1,15 +1,18 @@
 "use client";
 
-import useSWR from "swr";
+import useSWR, { SWRConfiguration } from "swr";
 
 import { SearchPalette } from "@/components/search-palette/search-palette";
 
 export function SearchContainer() {
-  const { data, error } = useSWR("search-data", fetchSearchData);
+  const options: SWRConfiguration = {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+  };
+  const { data, error } = useSWR("search-data", fetchSearchData, options);
 
-  if (error) return <div className="text-xs">Unable to load data</div>;
-  if (!data)
-    return <div className="text-xs text-muted-foreground">Loading</div>;
+  if (error) return <div className="text-xs">Error</div>;
+  if (!data) return <div className="text-xs text-muted-foreground">...</div>;
 
   return <SearchPalette allProjects={data.projects} allTags={data.tags} />;
 }
