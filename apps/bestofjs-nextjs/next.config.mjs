4653060d17e7245ceee3061feee338773be1058b
michaelrambeau <mikeair@gmail.com>
2023-09-10 21:08:08 +0900

    Handle redirect from `/ranking/monthly` in app config to avoid 404 error

--- apps/bestofjs-nextjs/next.config.mjs
+++ apps/bestofjs-nextjs/next.config.mjs
@@ -12,13 +12,39 @@ const nextConfig = {
       },
     ],
   },
-  redirects: () => [
-    {
-      source: "/tags/:tag",
-      destination: "/projects?tags=:tag",
-      permanent: false,
-    },
-  ],
+  redirects: async () => {
+    // TODO: how to re-use in this `.mjs` file the code from the app/ranking/monthly pages?
+    const monthlyRankingsRedirect = await getMonthlyRankingsRedirect();
+    return [
+      {
+        source: "/tags/:tag",
+        destination: "/projects?tags=:tag",
+        permanent: false,
+      },
+      monthlyRankingsRedirect,
+    ];
+  },
 };
 
+async function getMonthlyRankingsRedirect() {
+  const { year, month } = await fetchLatestRankings();
+  return {
+    source: "/rankings/monthly",
+    destination: `/rankings/monthly/${year}/${month}`,
+    permanent: false,
+  };
+}
+
+function fetchLatestRankings() {
+  const url = "https://bestofjs-rankings.vercel.app/monthly/latest"; //TODO URL should be setup in one single place
+  const data = fetch(url).then((res) => res.json());
+  return data;
+}
+
 export default nextConfig;
+
+// {
+//   source: "/tags/:tag",
+//   destination: "/projects?tags=:tag",
+//   permanent: false,
+// },
--- apps/bestofjs-nextjs/src/app/rankings/monthly/route.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-
-import { fetchMonthlyRankings } from "./monthly-rankings-data";
-
-export async function GET(request: Request) {
-  const { year, month } = await fetchMonthlyRankings({ limit: 0 });
-  redirect(`/rankings/monthly/${year}/${month}`);
-}
