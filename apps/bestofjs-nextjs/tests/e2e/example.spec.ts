122c913f7877f9db3280d65983cc7be5d2449fbb
Mihai Badescu <zawarudo@users.noreply.github.com>
2023-10-19 19:06:15 +0800

    feat(e2e): add testing commands, change naming of pull-request-check.yml to reflect adding e2e, rewrite example e2e test for vitest

--- apps/bestofjs-nextjs/tests/e2e/example.spec.ts
+++ apps/bestofjs-nextjs/tests/e2e/example.spec.ts
@@ -1,18 +1,38 @@
-import { test, expect } from '@playwright/test';
+import { Browser, BrowserContext, chromium, expect, Page } from "@playwright/test";
+import { afterAll, beforeAll, describe, test } from "vitest";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+describe("playwright meets vitest", () => {
+  let page: Page;
+  let browser: Browser;
+  let context: BrowserContext;
+  beforeAll(async () => {
+    browser = await chromium.launch();
+    context = await browser.newContext();
+    page = await context.newPage();
+  });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  test("has title", async () => {
+    await page.goto("https://playwright.dev");
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  });
+
+  test("get started link", async () => {
+    await page.goto("https://playwright.dev");
+
+    // Click the get started link.
+    await page.getByRole("link", { name: "Get started" }).click();
+
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL(/.*intro/);
+  });
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+  // // Expects page to have a heading with the name of Installation.
+  // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
