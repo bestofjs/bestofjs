8e96229e7ffc1ec1f603eb261c5439d887e782b8
michaelrambeau <mikeair@gmail.com>
2023-10-20 09:10:33 +0900

    Remove `sleep` function used to slow down intentionally the search

--- apps/bestofjs-nextjs/src/app/hall-of-fame/actions.ts
+++ apps/bestofjs-nextjs/src/app/hall-of-fame/actions.ts
@@ -5,10 +5,5 @@ import { api } from "@/server/api";
 export async function searchHallOfFameMembers(query: string) {
   "use server";
   const { members } = await api.hallOfFame.findMembers({ query });
-  await sleep(1000);
   return members;
-}
-
-function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
 }
