32be569c419401a287455972f9841eb05e61c311
michaelrambeau <mikeair@gmail.com>
2023-10-21 14:36:27 +0900

    Add comments for a better autocomplete

--- apps/bestofjs-nextjs/src/env.mjs
+++ apps/bestofjs-nextjs/src/env.mjs
@@ -3,14 +3,17 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
+    /** The huge JSON file with aggregated data about all projects tracked by Best of JS */
     STATIC_API_ROOT_URL: z
       .string()
       .url()
       .default("https://bestofjs-static-api.vercel.app"),
+    /** Serverless functions related to project details */
     PROJECT_DETAILS_API_ROOT_URL: z
       .string()
       .url()
       .default("https://bestofjs-serverless.vercel.app"),
+    /** Monthly rankings data */
     RANKINGS_ROOT_URL: z
       .string()
       .url()
