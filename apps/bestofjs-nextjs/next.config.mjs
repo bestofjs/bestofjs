ee90d4d1cd9bf5021ca04bcda9fc2a615366ecb6
michaelrambeau <mikeair@gmail.com>
2023-10-22 13:40:52 +0900

    Setup redirect from `/rankings/monthly/` in config file

--- apps/bestofjs-nextjs/next.config.mjs
+++ apps/bestofjs-nextjs/next.config.mjs
@@ -1,4 +1,4 @@
-import "./src/env.mjs";
+import { env } from "./src/env.mjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -14,13 +14,28 @@ const nextConfig = {
       },
     ],
   },
-  redirects: () => [
-    {
-      source: "/tags/:tag",
-      destination: "/projects?tags=:tag",
-      permanent: false,
-    },
-  ],
+  redirects: async () => {
+    const { year, month } = await fetchLatestRankings();
+    return [
+      {
+        source: "/tags/:tag",
+        destination: "/projects?tags=:tag",
+        permanent: false,
+      },
+      {
+        source: "/rankings/monthly",
+        destination: `/rankings/monthly/${year}/${month}`,
+        permanent: false,
+      },
+    ];
+  },
 };
+
+function fetchLatestRankings() {
+  const url = `${env.RANKINGS_ROOT_URL}/monthly/latest`;
+  console.log("Fetching latest rankings from", url);
+  const data = fetch(url).then((res) => res.json());
+  return data;
+}
 
 export default nextConfig;
