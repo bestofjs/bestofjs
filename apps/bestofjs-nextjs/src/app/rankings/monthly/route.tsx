283c6af77574adb39425b3f027fb88ebe269f64b
Michael Rambeau <mikeair@gmail.com>
2023-10-22 14:06:39 +0900

    Fix 404 error in the console because of redirect (#227)
    * Setup redirect from `/rankings/monthly/` in config file
    
    * Use T3 env on build script, for consistency

--- apps/bestofjs-nextjs/src/app/rankings/monthly/route.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-
-import { fetchMonthlyRankings } from "./monthly-rankings-data";
-
-export async function GET() {
-  const { year, month } = await fetchMonthlyRankings({ limit: 0 });
-  return redirect(`/rankings/monthly/${year}/${month}`);
-}
