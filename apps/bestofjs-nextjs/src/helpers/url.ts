b532d4dcf7c2cf9d697be470dd613ad1c52fe9ad
michaelrambeau <mikeair@gmail.com>
2023-10-30 10:47:20 +0900

    Add cache busting param to OG image URL, for project list and project details

--- apps/bestofjs-nextjs/src/helpers/url.ts
+++ apps/bestofjs-nextjs/src/helpers/url.ts
@@ -3,3 +3,16 @@ export default function formatUrl(url: string) {
   const result = url.replace(/\/$/, "").toLowerCase();
   return result.replace(/^https?:\/\/(.*)$/, "$1");
 }
+
+/**
+ * Add `&t=2024-01-01T06-00` to the URL search params
+ * Some URL need to reflect the date when JSON data is updated,
+ * to avoid caching issues (E.g. OG images)
+ */
+export function addCacheBustingParam(
+  searchParams: URLSearchParams,
+  date: Date
+) {
+  const dateParam = date.toISOString().slice(0, 16).replace(":", "-"); // 2020-01-01T00:00 => 2020-01-01T00-00
+  searchParams.set("t", dateParam);
+}
