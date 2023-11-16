45fea553720a0dc9390a666e685f5b4f8250e3a7
michaelrambeau <mikeair@gmail.com>
2023-11-16 21:14:49 +0900

    Change GitHub and Discord icons

--- apps/bestofjs-nextjs/src/components/tag-list/compact-tag-list.tsx
+++ apps/bestofjs-nextjs/src/components/tag-list/compact-tag-list.tsx
@@ -4,7 +4,7 @@ export function CompactTagList({ tags }: { tags: BestOfJS.Tag[] }) {
   return (
     <div className="divide-y">
       {tags.map((tag) => (
-        <div key={tag.code} className="px-4 py-2 hover:bg-muted/50">
+        <div key={tag.code} className="px-4 py-2 hover:bg-[var(--sand-1)]">
           <Link
             href={`/projects/?tags=${tag.code}`}
             className="flex justify-between"
