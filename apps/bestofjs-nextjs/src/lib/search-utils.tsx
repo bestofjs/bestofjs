d30234849d4418502dbc3f516cf6d7a3dd8e43f9
michaelrambeau <mikeair@gmail.com>
2023-11-25 16:23:14 +0900

    Speed up search queries

--- apps/bestofjs-nextjs/src/lib/search-utils.tsx
+++ apps/bestofjs-nextjs/src/lib/search-utils.tsx
@@ -1,4 +1,5 @@
-const DEBUG_MODE = false; // to show time spent in search functions
+const DEBUG_MODE = true; // to show time spent in search functions
+
 /**
  * Filter all projects when the user enters text in the search box
  * assigning a "rank" to each project.
@@ -8,86 +9,90 @@ export function filterProjectsByQuery<
   T extends Omit<BestOfJS.SearchIndexProject, "slug">
 >(projects: T[], query: string): Array<T & { rank: number }> {
   if (DEBUG_MODE) console.time(`Search projects "${query}"`);
-  const results = orderByRank(
-    projects
-      .map((project) => ({ ...project, rank: rank(project, query) }))
-      .filter((project) => project.rank > 0)
-  );
+  const results = orderByRank(rankAndFilterProjects(projects, query));
   if (DEBUG_MODE) console.timeEnd(`Search projects "${query}"`);
   return results;
 }
 
-// for a given project and a query,
-// return how much "relevant" is the project in the search results
-// `tags` is an array of tags that match the text
-function rank<T extends Omit<BestOfJS.SearchIndexProject, "slug">>(
-  project: T,
-  query: string
-) {
+function rankAndFilterProjects<
+  T extends Omit<BestOfJS.SearchIndexProject, "slug">
+>(projects: Array<T>, query: string) {
   const escapedQuery = escapeRegExp(query);
   const equals = new RegExp("^" + escapedQuery + "$", "i");
   const startsWith = new RegExp("^" + escapedQuery, "i");
   const contains = new RegExp(escapedQuery.replace(/ /g, ".+"), "i"); // the query is split if it contains spaces
 
-  if (equals.test(project.name)) {
-    // top level relevance: project whose name or package name is what the user entered
-    return 1;
-  }
+  return rankAndFilterItems(projects, rankProject);
 
-  if (startsWith.test(project.name)) {
-    return 0.7;
-  }
-
-  if (query.length > 1) {
-    if (contains.test(project.name)) {
-      return 0.5;
+  // for a given project and a query,
+  // return how much "relevant" is the project in the search results
+  // `tags` is an array of tags that match the text
+  function rankProject<T extends Omit<BestOfJS.SearchIndexProject, "slug">>(
+    project: T
+  ) {
+    if (equals.test(project.name)) {
+      // top level relevance: project whose name or package name is what the user entered
+      return 1;
     }
-  }
 
-  if (query.length > 2) {
-    if (contains.test(project.description)) {
-      return 0.4;
+    if (startsWith.test(project.name)) {
+      return 0.7;
     }
-    if (contains.test(project.full_name)) {
-      return 0.2;
-    }
-  }
 
-  // by default: the project is not included in search results
-  return 0;
+    if (query.length > 1) {
+      if (contains.test(project.name)) {
+        return 0.5;
+      }
+    }
+
+    if (query.length > 2) {
+      if (contains.test(project.description)) {
+        return 0.4;
+      }
+      if (contains.test(project.full_name)) {
+        return 0.2;
+      }
+    }
+
+    // by default: the project is not included in search results
+    return 0;
+  }
 }
 
-export function filterTagsByQueryWithRank(tags: BestOfJS.Tag[], query: string) {
+export function filterTagsByQueryWithRank(
+  tags: BestOfJS.Tag[],
+  query: string
+): Array<T & { rank: number }> {
   if (DEBUG_MODE) console.time(`Search tags "${query}"`);
-  const foundTags = orderByRank(
-    tags
-      .map((tag) => ({ ...tag, rank: rankTags(tag, query) }))
-      .filter((tag) => tag.rank > 0)
-  );
+  const foundTags = orderByRank(rankAndFilterTags(tags, query));
   if (DEBUG_MODE) console.timeEnd(`Search tags "${query}"`);
   return foundTags;
 }
 
-function rankTags(tag: BestOfJS.Tag, query: string) {
+function rankAndFilterTags<T extends BestOfJS.Tag>(tags: T[], query: string) {
   const escapedQuery = escapeRegExp(query);
   const equals = new RegExp("^" + escapedQuery + "$", "i");
   const startsWith = new RegExp("^" + escapedQuery, "i");
   const contains = new RegExp(escapedQuery.replace(/ /g, ".+"), "i"); // the query is split if it contains spaces
 
-  if (equals.test(tag.name) || equals.test(tag.code)) {
-    return 1;
-  }
-  if (
-    query.length > 1 &&
-    (startsWith.test(tag.name) || startsWith.test(tag.code))
-  ) {
-    return 0.8;
-  }
-  if (contains.test(tag.name) || contains.test(tag.code)) {
-    return 0.6;
-  }
+  return rankAndFilterItems(tags, rankTag);
 
-  return 0;
+  function rankTag(tag: BestOfJS.Tag) {
+    if (equals.test(tag.name) || equals.test(tag.code)) {
+      return 1;
+    }
+    if (
+      query.length > 1 &&
+      (startsWith.test(tag.name) || startsWith.test(tag.code))
+    ) {
+      return 0.8;
+    }
+    if (contains.test(tag.name) || contains.test(tag.code)) {
+      return 0.6;
+    }
+
+    return 0;
+  }
 }
 
 export function mergeSearchResults<T extends { rank: number }>(
@@ -151,4 +156,15 @@ function orderByFn<T>(items: T[], fn: (item: T) => number) {
 // From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
 function escapeRegExp(input: string) {
   return input.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
+}
+
+function rankAndFilterItems<T>(items: T[], rankFn: (item: T) => number) {
+  const results: Array<T & { rank: number }> = [];
+  for (const item of items) {
+    const rank = rankFn(item);
+    if (rank > 0) {
+      results.push({ ...item, rank });
+    }
+  }
+  return results;
 }
