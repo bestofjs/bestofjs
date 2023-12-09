9597ec8d84e10de41976c1530966b61a0ef015b8
Evan Trujillo <evantrujillo30@gmail.com>
2023-12-09 16:09:14 +0900

    Adjust api/tags/[slug] endpoint

--- apps/bestofjs-nextjs/src/app/api/tags/[slug]/route.tsx
+++ apps/bestofjs-nextjs/src/app/api/tags/[slug]/route.tsx
@@ -4,9 +4,9 @@ export const runtime = "edge";
 
 type Context = { params: { slug: string } };
 export async function GET(_req: Request, { params: { slug } }: Context) {
-  const { tags } = await api.tags.getTagBySlug({ criteria: { code: slug } });
+  const { tags } = await api.tags.getTagBySlug(slug);
   const output = { tags };
-  console.log("TAGS", tags)
+
   return new Response(JSON.stringify(output), {
     status: 200,
     headers: {
