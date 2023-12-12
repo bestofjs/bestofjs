23f1484d20f8625699005b60bf92f10ae4e54867
Evan <evantrujillo30@gmail.com>
2023-12-12 13:48:34 +0900

    WIP: refactor hover card component

--- apps/bestofjs-nextjs/src/components/tags/project-tag-hover-card.tsx
+++ apps/bestofjs-nextjs/src/components/tags/project-tag-hover-card.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import { useState } from "react";
 import useSWR, { SWRConfiguration } from "swr";
-import { Skeleton } from "../ui/skeleton";
 
 import { TagIcon } from "../core";
 import {
@@ -9,6 +9,7 @@ import {
   HoverCardContent,
   HoverCardTrigger,
 } from "../ui/hover-card";
+import { Skeleton } from "../ui/skeleton";
 
 export const ProjectTagHoverCard = ({
   children,
@@ -17,39 +18,43 @@ export const ProjectTagHoverCard = ({
   children: React.ReactNode;
   tag: BestOfJS.Tag;
 }) => {
-  const options: SWRConfiguration = {
-    revalidateIfStale: false,
-    revalidateOnFocus: false,
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const onOpenChange = (e) => {
+    setIsOpen(e)
   };
 
-  function fetchData() {
-    const url = `/api/tags/${tag.code}`;
-    return fetch(url).then((res) => res.json());
-  }
-
-  const { data, error } = useSWR(tag.code, fetchData, options);
-
   return (
-    <HoverCard>
+    <HoverCard onOpenChange={onOpenChange}>
       <HoverCardTrigger asChild>{children}</HoverCardTrigger>
-      <HoverCardContent className="w-80">
-        <div className="flex justify-between space-x-4">
-          <div className="space-y-1">
-            <div className="flex items-center space-x-1">
-              <div style={{ color: "#F59E0B" }}>
-                <TagIcon />
-              </div>
-              <h4 className="text-sm font-semibold">{tag.name}</h4>
-            </div>
-            <p className="text-sm ">{tag.description}</p>
-            <div className="flex-row gap-y-3">
-              <Skeleton className="h-[20px] w-[100%] p-4"></Skeleton>
-              <Skeleton className="h-[20px] w-[100%] p-4"></Skeleton>
-              <Skeleton className="h-[20px] w-[100%] p-4"></Skeleton>
-            </div>
-          </div>
-        </div>
+      <HoverCardContent>
+        <TagCardContent isOpen ={isOpen}/>
       </HoverCardContent>
     </HoverCard>
   );
 };
+
+
+
+const CardTagProjects = ({ isOpen }) => {
+  if (isOpen === true) {
+    console.log('it is open!', isOpen)
+
+  }
+};
+
+const TagCardContent = ({ isOpen }) => <CardTagProjects isOpen={isOpen} />;
+
+//TODO continue with component composition https://blog.logrocket.com/solving-prop-drilling-react-apps/
+
+// const options: SWRConfiguration = {
+//   revalidateIfStale: false,
+//   revalidateOnFocus: false,
+// };
+
+// function fetchData() {
+//   const url = `/api/tags/${tag.code}`;
+//   return fetch(url).then((res) => res.json());
+// }
+
+// const { data, error } = useSWR(tag.code, fetchData, options);
