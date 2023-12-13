19abd625d4a6a62ca832964df0c14741141299b9
Evan <evantrujillo30@gmail.com>
2023-12-13 17:37:56 +0900

    Breakdown hover card component into subcomponents

--- apps/bestofjs-nextjs/src/components/tags/project-tag-hover-card.tsx
+++ apps/bestofjs-nextjs/src/components/tags/project-tag-hover-card.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import useSWR, { SWRConfiguration } from "swr";
+
+import { ProjectAvatar } from "@/components/core";
 
 import { TagIcon } from "../core";
 import {
@@ -15,46 +17,106 @@ export const ProjectTagHoverCard = ({
   children,
   tag,
 }: {
-  children: React.ReactNode;
-  tag: BestOfJS.Tag;
+  children: ReactNode;
+  tag: BestOfJS.TagWithProjects;
 }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  const onOpenChange = (e) => {
-    setIsOpen(e)
+  const onOpenChange = (isOpen: boolean) => {
+    setIsOpen(isOpen);
   };
 
   return (
     <HoverCard onOpenChange={onOpenChange}>
       <HoverCardTrigger asChild>{children}</HoverCardTrigger>
-      <HoverCardContent>
-        <TagCardContent isOpen ={isOpen}/>
+      <HoverCardContent className="w-80">
+        <TagCardContent description={tag.description} name={tag.name}>
+          <CardTagProjects isOpen={isOpen} tag={tag} />
+        </TagCardContent>
       </HoverCardContent>
     </HoverCard>
   );
 };
 
+const CardTagProjects = ({
+  isOpen,
+  tag,
+}: {
+  isOpen: boolean;
+  tag: BestOfJS.TagWithProjects;
+}) => {
+  if (!isOpen) return;
 
-
-const CardTagProjects = ({ isOpen }) => {
-  if (isOpen === true) {
-    console.log('it is open!', isOpen)
-
-  }
+  return (
+    <div className="flex-col">
+      <h1 className="pb-1 text-left">Top 5 Projects</h1>
+      <FetchTagProjects tag={tag} />
+    </div>
+  );
 };
 
-const TagCardContent = ({ isOpen }) => <CardTagProjects isOpen={isOpen} />;
+const TagCardContent = ({
+  children,
+  description,
+  name,
+}: {
+  children: ReactNode;
+  description: string | undefined;
+  name: string;
+}) => (
+  <div className="flex justify-between space-x-4">
+    <div className="w-[100%] space-y-1">
+      <div className="flex items-center space-x-1">
+        <div style={{ color: "#F59E0B" }}>
+          <TagIcon />
+        </div>
+        <h4 className="text-sm font-semibold">{name}</h4>
+      </div>
+      <p className="text-left text-sm">{description}</p>
+      <div>
+        <span className="grow text-sm text-muted-foreground">{children}</span>
+      </div>
+    </div>
+  </div>
+);
+//TODO finish project list and cleanup
+const TagProjectList = ({ projects }: { projects: BestOfJS.Project[] }) => (
+  <div className="flex-col space-y-1">
+    {projects &&
+      projects.map((project) => (
+        <div className="flex">
+          <ProjectAvatar project={project} size={20} />
+          <div>{project.name}</div>
+        </div>
+      ))}
+  </div>
+);
 
-//TODO continue with component composition https://blog.logrocket.com/solving-prop-drilling-react-apps/
+const TagProjectListSkeleton = () => (
+  <div className="flex-col space-y-1">
+    <Skeleton className="h-6 w-[100%]" />
+    <Skeleton className="h-6 w-[100%]" />
+    <Skeleton className="h-6 w-[100%]" />
+    <Skeleton className="h-6 w-[100%]" />
+    <Skeleton className="h-6 w-[100%]" />
+  </div>
+);
 
-// const options: SWRConfiguration = {
-//   revalidateIfStale: false,
-//   revalidateOnFocus: false,
-// };
+const FetchTagProjects = ({ tag }: { tag: BestOfJS.TagWithProjects }) => {
+  const options: SWRConfiguration = {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+  };
 
-// function fetchData() {
-//   const url = `/api/tags/${tag.code}`;
-//   return fetch(url).then((res) => res.json());
-// }
+  function fetchTagData() {
+    const url = `/api/tags/${tag.code}`;
+    return fetch(url).then((res) => res.json());
+  }
+  const { data, error, isLoading } = useSWR(tag.code, fetchTagData, options);
+  if (error) return <div>Unable to fetch...</div>;
+  if (isLoading) return <TagProjectListSkeleton />;
 
-// const { data, error } = useSWR(tag.code, fetchData, options);
+  const { tags } = data;
+
+  return <TagProjectList projects={tags[0].projects} />;
+};
