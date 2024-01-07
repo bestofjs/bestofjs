e85d920b54d34203ef06388450eb280fe13e5e5e
remy sabliet <sabliet@gmail.com>
2024-01-07 15:28:47 +0900

    feat: Add hover card on Popular Tags section in the home page  (#266)
    * feat: add project-tag-hover-card on popular tags list
    
    * fix: move key attribute and use alias for Featured proj

--- apps/bestofjs-nextjs/src/components/tag-list/compact-tag-list.tsx
+++ apps/bestofjs-nextjs/src/components/tag-list/compact-tag-list.tsx
@@ -1,21 +1,22 @@
 import Link from "next/link";
 
+import { ProjectTagHoverCard } from "@/components/tags/project-tag-hover-card";
+
 export function CompactTagList({ tags }: { tags: BestOfJS.Tag[] }) {
   return (
     <div className="divide-y">
       {tags.map((tag) => (
-        <div
-          key={tag.code}
-          className="px-4 py-2 hover:bg-accent hover:text-accent-foreground"
-        >
-          <Link
-            href={`/projects/?tags=${tag.code}`}
-            className="flex justify-between"
-          >
-            <span>{tag.name}</span>
-            <span className="text-muted-foreground">{tag.counter}</span>
-          </Link>
-        </div>
+        <ProjectTagHoverCard key={tag.code} tag={tag}>
+          <div className="px-4 py-2 hover:bg-accent hover:text-accent-foreground">
+            <Link
+              href={`/projects/?tags=${tag.code}`}
+              className="flex justify-between"
+            >
+              <span>{tag.name}</span>
+              <span className="text-muted-foreground">{tag.counter}</span>
+            </Link>
+          </div>
+        </ProjectTagHoverCard>
       ))}
     </div>
   );
