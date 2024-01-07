dd146b84e4d940cf99f8a905aeb10a967e57f4e4
Rémy Sabliet <sabliet@gmail.com>
2024-01-07 12:00:17 +0900

    feat: add project-tag-hover-card on popular tags list

--- apps/bestofjs-nextjs/src/components/tag-list/compact-tag-list.tsx
+++ apps/bestofjs-nextjs/src/components/tag-list/compact-tag-list.tsx
@@ -1,21 +1,25 @@
 import Link from "next/link";
 
+import { ProjectTagHoverCard } from "../tags/project-tag-hover-card";
+
 export function CompactTagList({ tags }: { tags: BestOfJS.Tag[] }) {
   return (
     <div className="divide-y">
       {tags.map((tag) => (
-        <div
-          key={tag.code}
-          className="px-4 py-2 hover:bg-accent hover:text-accent-foreground"
-        >
-          <Link
-            href={`/projects/?tags=${tag.code}`}
-            className="flex justify-between"
+        <ProjectTagHoverCard tag={tag}>
+          <div
+            key={tag.code}
+            className="px-4 py-2 hover:bg-accent hover:text-accent-foreground"
           >
-            <span>{tag.name}</span>
-            <span className="text-muted-foreground">{tag.counter}</span>
-          </Link>
-        </div>
+            <Link
+              href={`/projects/?tags=${tag.code}`}
+              className="flex justify-between"
+            >
+              <span>{tag.name}</span>
+              <span className="text-muted-foreground">{tag.counter}</span>
+            </Link>
+          </div>
+        </ProjectTagHoverCard>
       ))}
     </div>
   );
