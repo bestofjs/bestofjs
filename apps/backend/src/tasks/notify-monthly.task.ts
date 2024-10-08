b49b807fab72184589201b8677bd1f835f8aa74b
michaelrambeau <mikeair@gmail.com>
2024-10-08 20:39:29 +0900

    Feat(backend) Add `notify-monthly` task

---
+++ apps/backend/src/tasks/notify-monthly.task.ts
@@ -0,0 +1,162 @@
+import debugPackage from "debug";
+import { z } from "zod";
+
+import { Task } from "@/task-runner";
+import { ProjectItem } from "./static-api-types";
+
+interface Project extends ProjectItem {
+  delta: number;
+}
+
+type RankingsData = {
+  year: number;
+  month: number;
+  trending: Project[];
+};
+
+const NUMBER_OF_PROJECTS = 5;
+
+const schema = z.object({ year: z.number(), month: z.number() });
+
+const debug = debugPackage("notify");
+
+export const notifyMonthlyTask: Task<z.infer<typeof schema>> = {
+  name: "notify-monthly",
+  description:
+    "Send notification on Discord after monthly rankings are published",
+
+  flags: {
+    year: { type: Number },
+    month: { type: Number },
+  },
+  schema,
+
+  run: async (context, flags) => {
+    const { dryRun, logger } = context;
+    const { year, month } = flags;
+    const discordURL = process.env.DISCORD_MONTHLY_WEBHOOK;
+    if (!discordURL)
+      throw new Error('No "DISCORD_MONTHLY_WEBHOOK" env. variable!');
+
+    const projects = await fetchMonthlyRankings(year, month);
+
+    logger.info(
+      "Sending the daily notifications...",
+      projects.map((project) => `${project.name}: +${project.delta}`)
+    );
+
+    if (
+      await notifyDiscord({ year, month, projects, url: discordURL, dryRun })
+    ) {
+      logger.info("Notification sent to Discord");
+    }
+    return { data: null, meta: { sent: true } };
+  },
+};
+
+async function fetchMonthlyRankings(year: number, month: number) {
+  const rootURL = process.env.RANKINGS_ROOT_URL;
+  if (!rootURL) throw new Error('No "RANKINGS_ROOT_URL" env. variable!');
+  const url = `${rootURL}/monthly/${formatDateForFilename(year, month)}`;
+  const data = (await fetch(url).then((res) => res.json())) as RankingsData;
+  const { trending } = data;
+  const projects = trending.slice(0, NUMBER_OF_PROJECTS);
+  return projects;
+}
+
+async function notifyDiscord({
+  projects,
+  month,
+  year,
+  url,
+  dryRun,
+}: {
+  month: number;
+  year: number;
+  projects: Project[];
+  url: string;
+  dryRun: boolean;
+}) {
+  const pageURL = `https://bestofjs.org/rankings/monthly/${year}/${month}`;
+  const text = [
+    `TOP ${NUMBER_OF_PROJECTS} Hottest Projects in ${formatMonth(year, month)}`,
+    `[Full rankings on Best of JS](${pageURL})`,
+  ].join("\n");
+  const colors = ["ffe38c", "ffae63", "f76d42", "d63c4a", "9c0042"]; // hex colors without the `#`
+
+  const embeds = projects.map((project, index) => {
+    const starsAdded = project.delta;
+    const text = `+${starsAdded} stars [number ${index + 1}]`;
+    const color = colors[index];
+    return projectToDiscordEmbed(project, text, color);
+  });
+
+  if (dryRun) {
+    console.info("[DRY RUN] No message sent to Discord", { text, embeds }); //eslint-disable-line no-console
+    return;
+  }
+
+  await sendMessageToDiscord(text, { url, embeds });
+  return true;
+}
+
+function projectToDiscordEmbed(project: Project, text: string, color: string) {
+  const url = project.url || `https://github.com/${project.full_name}`;
+  const thumbnailSize = 50;
+  return {
+    type: "article",
+    title: project.name,
+    url,
+    description: project.description,
+    thumbnail: {
+      url: `https://avatars.githubusercontent.com/u/${project.owner_id}?v=3&s=${thumbnailSize}`,
+      width: thumbnailSize,
+      height: thumbnailSize,
+    },
+    color: parseInt(color, 16), // has to be a decimal number
+    footer: { text }, // a header would be better to introduce the project
+  };
+}
+
+async function sendMessageToDiscord(
+  text: string,
+  { url, embeds }: { url: string; embeds: any }
+) {
+  const body = {
+    content: text,
+    embeds,
+  };
+  try {
+    debug(`Sending webhook to ${url}`, body);
+    const result = fetch(url, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: { "content-type": "application/json" },
+    }).then((res) => res.text());
+
+    debug("Response", result || "(No response)");
+    return true;
+  } catch (error) {
+    throw new Error(
+      `Invalid response from Discord ${(error as Error).message}`
+    );
+  }
+}
+
+/**
+ * @returns format a month as "2024/2024-10.json", to fetch data from the JSON API
+ */
+function formatDateForFilename(year: number, month: number) {
+  return year + "/" + year + "-" + month.toString().padStart(2, "0") + ".json";
+}
+
+/**
+ * @returns format a month as "October 2024"
+ */
+function formatMonth(year: number, month: number) {
+  const formatter = new Intl.DateTimeFormat("en-US", {
+    month: "long",
+    year: "numeric",
+  });
+  return formatter.format(new Date(year, month - 1));
+}
