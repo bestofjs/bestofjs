b52e50a4075a923572d2db6bf65986c95b68ed82
Michael Rambeau <mikeair@gmail.com>
2025-02-23 08:41:00 +0900

    Add database backup script (#344)
    * Add database backup script
    
    * Add backup script to package.json

---
+++ apps/backend/src/backup/make-backup.ts
@@ -0,0 +1,66 @@
+import { createWriteStream, existsSync, readdirSync } from "fs";
+import path from "path";
+import consola from "consola";
+import spawn from "nano-spawn";
+import prettyMs from "pretty-ms";
+
+main();
+
+/** Backup the database in `db-backup` folder at the root of the monorepo */
+async function main() {
+  const url = process.env.POSTGRES_URL;
+  if (!url) throw new Error("Missing POSTGRES_URL environment variable");
+  const nextBackupFilename = getNextBackupFilename();
+  console.log(nextBackupFilename);
+
+  const filepath = path.join(getFolderFullPath(), nextBackupFilename);
+  await launchBackupCommand(url, filepath);
+}
+
+async function launchBackupCommand(dbURL: string, filepath: string) {
+  consola.box("Backup...", filepath);
+  try {
+    const result = await spawn("pg_dump", [dbURL], {
+      stdout: createWriteStream(filepath),
+    });
+    consola.success("Backup done", prettyMs(result.durationMs));
+  } catch (error) {
+    consola.error("Backup failed", error);
+  }
+}
+
+function getFolderFullPath() {
+  const year = new Date().getFullYear();
+  return path.join(process.cwd(), "db-backup", year.toString());
+}
+
+function getNextBackupFilename() {
+  const nextBackupNumber = getNextBackupNumber();
+  const formattedNumber = nextBackupNumber.toString().padStart(3, "0");
+  return `backup-${formattedNumber}.sql`;
+}
+
+function getNextBackupNumber() {
+  const fileNames = getPreviousBackupFilenames();
+  const lastFileName = fileNames.at(-1);
+  if (!lastFileName) return 1;
+  const lastBackupNumber = extractBackupNumber(lastFileName);
+  if (!lastBackupNumber) {
+    throw new Error("Invalid backup filename");
+  }
+  return lastBackupNumber + 1;
+}
+
+function getPreviousBackupFilenames() {
+  const filepath = getFolderFullPath();
+  if (!existsSync(filepath))
+    throw new Error(`Backup folder not found: ${filepath}`);
+  const fileNames = readdirSync(filepath);
+  fileNames.sort();
+  return fileNames;
+}
+
+function extractBackupNumber(fileName: string): number | null {
+  const match = fileName.match(/backup-(\d+)\.sql/);
+  return match ? parseInt(match[1], 10) : null;
+}
