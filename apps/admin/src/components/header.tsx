7f2a4ccb349baf03b18696e7c6f90bd859064337
michaelrambeau <mikeair@gmail.com>
2025-04-20 11:49:12 +0900

    Fix warning about legacy attribute in Next.js link

--- apps/admin/src/components/header.tsx
+++ apps/admin/src/components/header.tsx
@@ -1,38 +1,29 @@
 "use client";
 
 import * as React from "react";
-import Link from "next/link";
-
+import NextLink, { type LinkProps } from "next/link";
+import { usePathname } from "next/navigation";
 import {
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
+} from "@radix-ui/react-navigation-menu";
 
 export function Header() {
   return (
     <header className="flex h-16 items-center border-b">
       <div className="container flex items-center gap-4">
-        <Link href="/" legacyBehavior passHref>
-          <a className="text-lg font-bold">Best of JS Admin</a>
-        </Link>
-        <NavigationMenu>
-          <NavigationMenuList>
+        <NextLink href="/" className="text-lg font-bold">
+          Best of JS Admin
+        </NextLink>
+        <NavigationMenu className="relative z-10 flex max-w-max flex-1 items-center justify-center">
+          <NavigationMenuList className="group flex flex-1 list-none items-center justify-center space-x-1">
             <NavigationMenuItem>
-              <Link href="/projects" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Projects
-                </NavigationMenuLink>
-              </Link>
+              <Link href="/projects">Projects</Link>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <Link href="/tags" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Tags
-                </NavigationMenuLink>
-              </Link>
+              <Link href="/tags">Tags</Link>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
@@ -40,3 +31,18 @@ export function Header() {
     </header>
   );
 }
+
+const Link = ({ href, ...props }: React.PropsWithChildren<LinkProps>) => {
+  const pathname = usePathname();
+  const isActive = href === pathname;
+
+  return (
+    <NavigationMenuLink
+      asChild
+      active={isActive}
+      className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+    >
+      <NextLink href={href} className="NavigationMenuLink" {...props} />
+    </NavigationMenuLink>
+  );
+};
