2910716628234a25f1a24cebd4b3438734ac16ed
michaelrambeau <mikeair@gmail.com>
2025-09-14 13:57:37 +0900

    Add data-table from https://www.diceui.com/docs/components/data-table Fix lint error about client events on div

---
+++ apps/admin/src/types/data-table.ts
@@ -0,0 +1,41 @@
+import type { ColumnSort, Row, RowData } from "@tanstack/react-table";
+
+import type { DataTableConfig } from "@/config/data-table";
+import type { FilterItemSchema } from "@/lib/parsers";
+
+declare module "@tanstack/react-table" {
+  // biome-ignore lint/correctness/noUnusedVariables: TValue is used in the ColumnMeta interface
+  interface ColumnMeta<TData extends RowData, TValue> {
+    label?: string;
+    placeholder?: string;
+    variant?: FilterVariant;
+    options?: Option[];
+    range?: [number, number];
+    unit?: string;
+    icon?: React.FC<React.SVGProps<SVGSVGElement>>;
+  }
+}
+
+export interface Option {
+  label: string;
+  value: string;
+  count?: number;
+  icon?: React.FC<React.SVGProps<SVGSVGElement>>;
+}
+
+export type FilterOperator = DataTableConfig["operators"][number];
+export type FilterVariant = DataTableConfig["filterVariants"][number];
+export type JoinOperator = DataTableConfig["joinOperators"][number];
+
+export interface ExtendedColumnSort<TData> extends Omit<ColumnSort, "id"> {
+  id: Extract<keyof TData, string>;
+}
+
+export interface ExtendedColumnFilter<TData> extends FilterItemSchema {
+  id: Extract<keyof TData, string>;
+}
+
+export interface DataTableRowAction<TData> {
+  row: Row<TData>;
+  variant: "update" | "delete";
+}
