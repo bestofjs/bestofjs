6f1f6912ffaea41a6dfd6d3efaf613039da7fbc6
michaelrambeau <mikeair@gmail.com>
2026-08-08 17:08:58 +0900

    Improve laoding of `/featured` page

--- apps/web/src/app/featured/loading.tsx
+++ apps/web/src/app/featured/loading.tsx
@@ -1,3 +1,16 @@
+import { ProjectListCardLoading } from "@/app/projects/loading-state";
+import { StarIcon } from "@/components/core";
+import { PageHeading } from "@/components/core/typography";
+
 export default function FeaturedProjectsLoading() {
-  return <>Loading Featured Projects</>;
+  return (
+    <>
+      <PageHeading
+        icon={<StarIcon className="size-9" />}
+        title={<>Featured Projects</>}
+        subtitle={<>A collection of awesome projects chosen by our team!</>}
+      />
+      <ProjectListCardLoading />
+    </>
+  );
 }
